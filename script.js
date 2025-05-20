fc230a4e00ddf6a8cd65647b26ab19fe4b85e98e
miguel salles <miguel.absalles@gmail.com>
2025-05-20 08:09:56 -0300

    msg de erro

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
+// add o form
 const form = document.getElementById("form");
 
+//add o event listener no form
 form.addEventListener("submit", (event) => {
- event.preventDefault();
-    isValidName ()
-    isValidPassword ()
- alert("CADASTRADO COM SUCESSO")
-})
+  event.preventDefault();
+  isValidName();
+  isValidPassword();
+  alert("CADASTRADO COM SUCESSO");
+});
 
 
+// pegando os inputs
+const name = document.getElementById("nameOrgan").value;
+const password = document.getElementById("passwordOrgan").value;
+const confirmPassword = document.getElementById("ConfirmPasswordOrgan").value;
 
-function isValidName () {
-    let name = document.getElementById("nameOrgan").value;
-    name = name.trim();
-    if (name != "" && name.length >=8 && name.length <=25 ) {
-        const validName = name
-        return true
-    } else {
-        msgError( "nameOrgan" , "O nome deve ter mais de 8 caracteres")
-        return false
-    }
-}
-//mensagem de erro
 
-function isValidPassword () {
-    let password = document.getElementById("passwordOrgan").value;
-    let confirmPassword = document.getElementById("ConfirmPasswordOrgan").value;
-    if (password == confirmPassword && password.length >=8 && password.length <=25) {
-        const validPassword = password;
-        return true
-    } else {
-        msgError(password , "As senhas devem ter mais de 8 caracteres e serem identicas")
-        return false
-    }
+//validando os dados
+function isValidName() {
+  let name = document.getElementById("nameOrgan").value;
+  name = name.trim();
+  if (name != "" && name.length >= 8 && name.length <= 25) {
+    const validName = name;
+    return true;
+  } else {
+    msgError("nameOrgan", "O nome deve ter mais de 8 caracteres");
+    return false;
+  }
 }
 
-function msgError (input , message) {
-    const formItem = input.parentElement;
-    const  textMsg = formItem.querySelector("p")
-    
-    textMsg.innerHTML = message;
-    formItem.className = "error";
-    textMsg.style.display = "block";
+function isValidPassword() {
+  let password = document.getElementById("passwordOrgan").value;
+  let confirmPassword = document.getElementById("ConfirmPasswordOrgan").value;
+  if (
+    password == confirmPassword &&
+    password.length >= 8 &&
+    password.length <= 25
+  ) {
+    const validPassword = password;
+    return true;
+  } else {
+    msgError(
+      (passwordOrgan,confirmPasswordOrgan),
+      "As senhas devem ter mais de 8 caracteres e serem identicas"
+    );
+    return false;
+  }
 }
+
+function msgError(input, message) {
+  const formItem = input.parentElement;
+  const textMsg = formItem.querySelector("p");
+
+  textMsg.innerHTML = message;
+  formItem.className = "error";
+  textMsg.style.display = "block";
+}
+
+
+
+
+
+const info = {
+  nome:name
+};
+
+const infoJSON = JSON.stringify();
